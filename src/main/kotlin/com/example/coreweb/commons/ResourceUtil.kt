bb34d34fe733512c406e9adf11697bb6a33e022a
Sayem Hossain <sayemkcn@gmail.com>
2021-01-10 10:24:00 +0600

    [Add] resourceutil for file downloads

---
+++ src/main/kotlin/com/example/coreweb/commons/ResourceUtil.kt
@@ -0,0 +1,34 @@
+package com.example.coreweb.commons
+
+import org.apache.commons.io.FilenameUtils
+import org.springframework.core.io.InputStreamResource
+import org.springframework.core.io.Resource
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import java.io.File
+import java.io.FileInputStream
+
+class ResourceUtil private constructor() {
+
+    companion object {
+
+        fun buildFileDownloadResponse(filePath: String, fileName: String): ResponseEntity<Resource> {
+            val file = File(filePath)
+            return buildDownloadResponse(file, fileName)
+        }
+
+        fun buildDownloadResponse(file: File, fileName: String): ResponseEntity<Resource> {
+            val resource = InputStreamResource(FileInputStream(file))
+            val headers = HttpHeaders()
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName + "." + FilenameUtils.getExtension(file.name))
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .contentLength(file.length())
+                    .contentType(MediaType.parseMediaType("application/octet-stream"))
+                    .body(resource)
+        }
+
+    }
+
+}
