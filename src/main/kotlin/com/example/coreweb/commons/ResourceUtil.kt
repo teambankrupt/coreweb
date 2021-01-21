ce2d5db4e723f38e543bd288bff70ab7204c0b66
razzak007 <razzak@cmedhealth.com>
2021-01-21 15:59:22 +0600

    remove email and phone number helper method from resourceUtil and use validator class

--- src/main/kotlin/com/example/coreweb/commons/ResourceUtil.kt
+++ src/main/kotlin/com/example/coreweb/commons/ResourceUtil.kt
@@ -14,21 +14,6 @@ class ResourceUtil private constructor() {
 
     companion object {
 
-        private const val phoneNumberRegexBD = "\\+?(88)?0?1[56789][0-9]{8}\\b" // this is for bangladeshi phone number
-        private const val phoneNumberRegex = "^\\+(?:[0-9]●?){6,14}[0-9]\$"
-        private const val emailRegex = "^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})\$"
-
-
-        @JvmStatic
-        fun isEmailValid(email: String): Boolean {
-            return Pattern.compile(emailRegex).matcher(email).matches()
-        }
-
-        @JvmStatic
-        fun isPhoneNumberValid(phoneNumber: String): Boolean {
-            return Pattern.compile(phoneNumberRegex).matcher(phoneNumber).matches()
-        }
-
         fun buildFileDownloadResponse(filePath: String, fileName: String): ResponseEntity<Resource> {
             val file = File(filePath)
             return buildDownloadResponse(file, fileName)
