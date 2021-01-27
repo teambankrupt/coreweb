fe548883d2a1d68cc81b320956c33f364793c6ee
Sayem Hossain <sayemkcn@gmail.com>
2021-01-27 23:23:20 +0600

    [Fix] location create issue

--- src/main/kotlin/com/example/coreweb/domains/locationtypes/services/beans/LocationTypeServiceBean.kt
+++ src/main/kotlin/com/example/coreweb/domains/locationtypes/services/beans/LocationTypeServiceBean.kt
@@ -54,7 +54,7 @@ class LocationTypeServiceBean @Autowired constructor(
         }
 
         // check if entity is a parent of it's own
-        if (entity.isNew) {
+        if (!entity.isNew) {
             if (entity.parent?.id == entity.id)
                 throw ExceptionUtil.forbidden("${entity.label} can not be it's own parent.")
         }
