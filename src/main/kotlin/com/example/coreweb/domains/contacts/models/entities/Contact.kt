f7b053b4dc5ee0b9bab8e35d43328d98671a0ddf
Sayem Hossain <sayemkcn@gmail.com>
2021-02-01 21:58:38 +0600

    [Fix] contact addresses

--- src/main/kotlin/com/example/coreweb/domains/contacts/models/entities/Contact.kt
+++ src/main/kotlin/com/example/coreweb/domains/contacts/models/entities/Contact.kt
@@ -2,8 +2,6 @@ package com.example.coreweb.domains.contacts.models.entities
 
 import com.example.coreweb.domains.base.entities.BaseEntity
 import com.example.coreweb.domains.globaladdresss.models.entities.GlobalAddress
-import org.hibernate.annotations.Cascade
-import org.hibernate.annotations.CascadeType
 import org.hibernate.annotations.LazyCollection
 import org.hibernate.annotations.LazyCollectionOption
 import javax.persistence.*
@@ -16,14 +14,13 @@ class Contact : BaseEntity() {
     @Column(nullable = false)
     lateinit var name: String
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     lateinit var phone: String
 
     var email: String? = null
 
     @OneToMany
     @LazyCollection(LazyCollectionOption.FALSE)
-    @Cascade(CascadeType.ALL)
     @JoinTable(name = "contact_addresses", schema = "core_web")
     var address: MutableList<GlobalAddress>? = null
 }
