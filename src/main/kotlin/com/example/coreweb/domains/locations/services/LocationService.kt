c80efb40dbddd369f4ede5a6ffb6d75fb88750ff
Abdur Razzak <abdur.razzaq06@gmail.com>
2021-07-25 17:02:32 +0600

    #20 added zip code field on location entity so that we can easily found location information using zip code (#4)
    Co-authored-by: razzak007 <razzak@cmedhealth.com>

--- src/main/kotlin/com/example/coreweb/domains/locations/services/LocationService.kt
+++ src/main/kotlin/com/example/coreweb/domains/locations/services/LocationService.kt
@@ -5,8 +5,10 @@ import com.example.coreweb.domains.locations.models.entities.Location
 import com.example.coreweb.domains.base.services.CrudServiceV2
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Sort
+import java.util.*
 
 interface LocationService : CrudServiceV2<Location> {
     fun searchForType(typeId: Long, query: String, page: Int, size: Int, sortBy: SortByFields, direction: Sort.Direction): Page<Location>
     fun searchByParent(parentId: Long?, query: String, page: Int, size: Int, sortBy: SortByFields, direction: Sort.Direction): Page<Location>
+    fun searchByZipCode(zipCode: String): Optional<Location>
 }
