3c7210d5435e8b4c0ced6e9bb79d448512361966
Sayem Hossain <sayemkcn@gmail.com>
2021-11-03 15:47:12 +0600

    [Fix] base dto items made readonly

--- src/main/kotlin/com/example/coreweb/domains/base/models/dtos/BaseDto.kt
+++ src/main/kotlin/com/example/coreweb/domains/base/models/dtos/BaseDto.kt
@@ -9,16 +9,24 @@ import java.io.Serializable
 import java.time.Instant
 
 open class BaseDto : Serializable {
-    @JsonProperty("id")
+    @JsonProperty("id", access = JsonProperty.Access.READ_ONLY)
     @ApiModelProperty(readOnly = true, example = "1")
     var id: Long? = null
 
-    @JsonProperty(value = "created_at")
-    @ApiModelProperty(readOnly = true, example = "2020-09-13T03:48:36Z", notes = "Date when this entity was first created. ( Format: yyyy-MM-dd'T'HH:mm:ss.SSS'Z' )")
+    @JsonProperty(value = "created_at", access = JsonProperty.Access.READ_ONLY)
+    @ApiModelProperty(
+        readOnly = true,
+        example = "2020-09-13T03:48:36Z",
+        notes = "Date when this entity was first created. ( Format: yyyy-MM-dd'T'HH:mm:ss.SSS'Z' )"
+    )
     lateinit var createdAt: Instant
 
-    @JsonProperty("updated_at")
-    @ApiModelProperty(readOnly = true,example = "2020-09-13T03:48:36Z", notes = "Date when this entity was last updated. ( Format: yyyy-MM-dd'T'HH:mm:ss.SSS'Z' )")
+    @JsonProperty("updated_at", access = JsonProperty.Access.READ_ONLY)
+    @ApiModelProperty(
+        readOnly = true,
+        example = "2020-09-13T03:48:36Z",
+        notes = "Date when this entity was last updated. ( Format: yyyy-MM-dd'T'HH:mm:ss.SSS'Z' )"
+    )
     lateinit var updatedAt: Instant
 
     @JsonIgnore
