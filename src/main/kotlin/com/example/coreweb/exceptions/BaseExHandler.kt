0b1f45a978f9e47a98b4164701607e0d8b6aa675
Sayem Hossain <mail@sayem.dev>
2023-09-30 00:52:01 +0600

    [Exception] handled new exception

--- src/main/kotlin/com/example/coreweb/exceptions/BaseExHandler.kt
+++ src/main/kotlin/com/example/coreweb/exceptions/BaseExHandler.kt
@@ -2,6 +2,7 @@ package com.example.coreweb.exceptions
 
 import com.example.common.exceptions.forbidden.ForbiddenException
 import com.example.common.exceptions.invalid.InvalidException
+import com.example.common.exceptions.notacceptable.NotAcceptableException
 import com.example.common.exceptions.notfound.NotFoundException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.env.Environment
@@ -9,11 +10,10 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import java.lang.RuntimeException
 
 @ControllerAdvice
 open class BaseExHandler @Autowired constructor(
-        private val env: Environment
+    private val env: Environment
 ) {
 
     fun debug(): Boolean {
@@ -31,6 +31,11 @@ open class BaseExHandler @Autowired constructor(
         return buildResponse(HttpStatus.FORBIDDEN, ex)
     }
 
+    @ExceptionHandler(NotAcceptableException::class)
+    fun handleNotAcceptableException(ex: NotAcceptableException): ResponseEntity<ErrorResponse> {
+        return buildResponse(HttpStatus.NOT_ACCEPTABLE, ex)
+    }
+
     @ExceptionHandler(InvalidException::class)
     fun handleInvalidException(ex: InvalidException): ResponseEntity<ErrorResponse> {
         return buildResponse(HttpStatus.BAD_REQUEST, ex)
@@ -43,18 +48,19 @@ open class BaseExHandler @Autowired constructor(
 
     fun buildResponse(status: HttpStatus, ex: Throwable): ResponseEntity<ErrorResponse> {
         val response = if (debug()) ErrorResponse(
-                status.value(),
-                status.name,
-                ex.message ?: "",
-                ex
+            status.value(),
+            status.name,
+            ex.message ?: "",
+            ex
         ) else ErrorResponse(
-                status.value(),
-                status.name,
-                ex.message ?: "")
+            status.value(),
+            status.name,
+            ex.message ?: ""
+        )
 
         return ResponseEntity
-                .status(status)
-                .body(response)
+            .status(status)
+            .body(response)
     }
 
 }
