2d3c9990dac254fcb2525fcfc0b23c83769df461
Sayem Hossain <mail@sayem.dev>
2024-01-15 19:30:33 +0600

    Implemented validation scopes

--- src/main/kotlin/com/example/coreweb/domains/base/services/CrudServiceV5.kt
+++ src/main/kotlin/com/example/coreweb/domains/base/services/CrudServiceV5.kt
@@ -4,11 +4,13 @@ import arrow.core.*
 import com.example.auth.config.security.SecurityContext
 import com.example.auth.entities.UserAuth
 import com.example.common.exceptions.Err
+import com.example.common.validation.ValidationScope
 import com.example.common.validation.ValidationV2
 import com.example.coreweb.domains.base.entities.BaseEntityV2
 import com.example.coreweb.utils.PageableParams
 import org.json.XMLTokener.entity
 import org.springframework.data.domain.Page
+import org.springframework.data.jpa.domain.AbstractPersistable_.id
 import org.springframework.data.jpa.repository.JpaRepository
 import java.time.Instant
 import javax.validation.ConstraintViolationException
@@ -17,7 +19,7 @@ interface CrudServiceV5<ENTITY : BaseEntityV2> {
 
     fun save(entity: ENTITY, asUser: UserAuth): Either<Err.ValidationErr, ENTITY> =
         this.validations(asUser)
-            .map { it.apply(entity) } // Apply all validations
+            .map { it.apply(entity, ValidationScope.Write) } // Apply all validations
             .firstOrNone { it.isLeft() } // Get the first error, if any of the validations failed
             .fold(
                 { entity.right() }, // No error
@@ -28,13 +30,24 @@ interface CrudServiceV5<ENTITY : BaseEntityV2> {
 
     fun validations(asUser: UserAuth): Set<ValidationV2<ENTITY>>
 
-    fun find(id: Long): Option<ENTITY>
+    fun find(id: Long, asUser: UserAuth): Option<ENTITY>
 
-    fun getAsEither(id: Long): Either<Err.OperationErr.NonExistentErr, ENTITY> = this.find(id)
-        .toEither { Err.OperationErr.NonExistentErr(id) }
+    fun getAsEither(id: Long, asUser: UserAuth): Either<Err, ENTITY> =
+        this.find(id = id, asUser = asUser)
+            .map { entity ->
+                this.validations(asUser)
+                    .map { it.apply(entity, ValidationScope.Read) }
+                    .firstOrNone { it.isLeft() }
+                    .fold(
+                        { entity.right() },
+                        { it }
+                    )
+            }
+            .toEither { Err.OperationErr.NonExistentErr(id) }
+            .flatten()
 
     fun delete(id: Long, softDelete: Boolean, asUser: UserAuth): Either<Err.OperationErr, Boolean> =
-        this.getAsEither(id)
+        this.getAsEither(id = id, asUser = asUser)
             .flatMap {
                 if (canAccess(it).not())
                     Err.OperationErr.ForbiddenErr.left()
