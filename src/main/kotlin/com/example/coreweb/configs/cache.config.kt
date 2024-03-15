9bfd7ae3f4c380b8fb1b5833efd36e1caeba15e3
Sayem Hossain <mail@sayem.dev>
2024-03-15 15:11:07 +0600

    Evicting cache daily

--- src/main/kotlin/com/example/coreweb/configs/cache.config.kt
+++ src/main/kotlin/com/example/coreweb/configs/cache.config.kt
@@ -5,6 +5,7 @@ import org.springframework.cache.annotation.EnableCaching
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.Scheduled
 
 
 @Configuration
@@ -12,10 +13,17 @@ import org.springframework.context.annotation.Configuration
 open class CacheConfig {
     companion object {
         const val CACHE_NAME = "bankrupt-cache"
+        const val CACHE_NAME_DAILY = "bankrupt-cache-daily"
     }
 
     @Bean
     open fun cacheManager(): CacheManager {
-        return ConcurrentMapCacheManager(CACHE_NAME)
+        return ConcurrentMapCacheManager(CACHE_NAME, CACHE_NAME_DAILY)
+    }
+
+    @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Dhaka")
+    fun evictDailyCache() {
+        cacheManager().getCache(CACHE_NAME_DAILY)?.clear()
+        println("Evicting daily cache..")
     }
 }
