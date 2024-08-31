27e084e58a41fc5fbe6c396f814c45abf7f0b170
Sayem Hossain <mail@sayem.dev>
2024-08-31 17:49:38 +0600

    Added PageableParamsV2

---
+++ src/main/kotlin/com/example/coreweb/utils/pageable-params.kt
@@ -0,0 +1,21 @@
+package com.example.coreweb.utils
+
+import com.example.coreweb.domains.base.entities.BaseEntityV2
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
+import kotlin.reflect.full.memberProperties
+
+data class PageableParamsV2(
+    val query: String?,
+    val page: Int,
+    val size: Int,
+    val sortBy: String,
+    val sortDirection: Sort.Direction,
+) {
+    inline fun <reified T : BaseEntityV2> toPageRequest(): PageRequest {
+        val sort = T::class.memberProperties.firstOrNull { it.name == sortBy }?.name ?: "id"
+        return PageRequest.of(
+            page, size, sortDirection, sort
+        )
+    }
+}
