a3720994fdffd9d73e68fada56443bb484f686c7
Sayem Hossain <mail@sayem.dev>
2024-09-19 01:46:40 +0600

    Added crudexamplev7

--- src/main/kotlin/com/example/coreweb/domains/base/controllers/crud.controller.kt
+++ src/main/kotlin/com/example/coreweb/domains/base/controllers/crud.controller.kt
@@ -10,68 +10,89 @@ import java.time.Instant
 
 @Deprecated("Use CrudControllerV5", ReplaceWith("CrudControllerV5<REQ, BRIEF_RESPONSE, DETAIL_RESPONSE>"))
 interface CrudControllerV4<REQ, BRIEF_RESPONSE, DETAIL_RESPONSE> {
-    fun search(
-        username: String?,
-        fromDate: Instant?, toDate: Instant?,
-        query: String?,
-        page: Int, size: Int,
-        sortBy: SortByFields,
-        direction: Sort.Direction
-    ): ResponseEntity<ResponseData<Page<BRIEF_RESPONSE>>>
+	fun search(
+		username: String?,
+		fromDate: Instant?, toDate: Instant?,
+		query: String?,
+		page: Int, size: Int,
+		sortBy: SortByFields,
+		direction: Sort.Direction
+	): ResponseEntity<ResponseData<Page<BRIEF_RESPONSE>>>
 
-    fun find(id: Long): ResponseEntity<ResponseData<DETAIL_RESPONSE>>
-    fun create(req: REQ): ResponseEntity<ResponseData<DETAIL_RESPONSE>>
-    fun update(id: Long, req: REQ): ResponseEntity<ResponseData<DETAIL_RESPONSE>>
-    fun delete(id: Long): ResponseEntity<ResponseData<Boolean>>
+	fun find(id: Long): ResponseEntity<ResponseData<DETAIL_RESPONSE>>
+	fun create(req: REQ): ResponseEntity<ResponseData<DETAIL_RESPONSE>>
+	fun update(id: Long, req: REQ): ResponseEntity<ResponseData<DETAIL_RESPONSE>>
+	fun delete(id: Long): ResponseEntity<ResponseData<Boolean>>
 
-    fun getEnv(): Environment
+	fun getEnv(): Environment
 
-    fun debug(): Boolean = getEnv().activeProfiles.contains("prod").not()
+	fun debug(): Boolean = getEnv().activeProfiles.contains("prod").not()
 
 }
 
 @Deprecated("Use CrudControllerV6", ReplaceWith("CrudControllerV6<REQ, BRIEF_RESPONSE, DETAIL_RESPONSE>"))
 interface CrudControllerV5<REQ, BRIEF_RESPONSE, DETAIL_RESPONSE> {
-    fun search(
-        username: String?,
-        fromDate: Instant?, toDate: Instant?,
-        query: String?,
-        page: Int, size: Int,
-        sortBy: SortByFields,
-        direction: Sort.Direction,
-        extra: Map<String, String>
-    ): ResponseEntity<ResponseData<Page<BRIEF_RESPONSE>>>
+	fun search(
+		username: String?,
+		fromDate: Instant?, toDate: Instant?,
+		query: String?,
+		page: Int, size: Int,
+		sortBy: SortByFields,
+		direction: Sort.Direction,
+		extra: Map<String, String>
+	): ResponseEntity<ResponseData<Page<BRIEF_RESPONSE>>>
 
-    fun find(id: Long): ResponseEntity<ResponseData<DETAIL_RESPONSE>>
-    fun create(req: REQ): ResponseEntity<ResponseData<DETAIL_RESPONSE>>
-    fun update(id: Long, req: REQ): ResponseEntity<ResponseData<DETAIL_RESPONSE>>
-    fun delete(id: Long): ResponseEntity<ResponseData<Boolean>>
+	fun find(id: Long): ResponseEntity<ResponseData<DETAIL_RESPONSE>>
+	fun create(req: REQ): ResponseEntity<ResponseData<DETAIL_RESPONSE>>
+	fun update(id: Long, req: REQ): ResponseEntity<ResponseData<DETAIL_RESPONSE>>
+	fun delete(id: Long): ResponseEntity<ResponseData<Boolean>>
 
-    fun getEnv(): Environment
+	fun getEnv(): Environment
 
-    fun debug(): Boolean = getEnv().activeProfiles.contains("prod").not()
+	fun debug(): Boolean = getEnv().activeProfiles.contains("prod").not()
 
 }
 
 interface CrudControllerV6<REQ, BRIEF_RESPONSE, DETAIL_RESPONSE> {
-    fun search(
-        username: String?,
-        fromDate: Instant?, toDate: Instant?,
-        query: String?,
-        page: Int, size: Int,
-        sortBy: String,
-        direction: Sort.Direction,
-        extra: Map<String, String>
-    ): ResponseEntity<ResponseData<Page<BRIEF_RESPONSE>>>
+	fun search(
+		username: String?,
+		fromDate: Instant?, toDate: Instant?,
+		query: String?,
+		page: Int, size: Int,
+		sortBy: String,
+		direction: Sort.Direction,
+		extra: Map<String, String>
+	): ResponseEntity<ResponseData<Page<BRIEF_RESPONSE>>>
 
-    fun find(id: Long): ResponseEntity<ResponseData<DETAIL_RESPONSE>>
-    fun create(req: REQ): ResponseEntity<ResponseData<DETAIL_RESPONSE>>
-    fun update(id: Long, req: REQ): ResponseEntity<ResponseData<DETAIL_RESPONSE>>
-    fun delete(id: Long): ResponseEntity<ResponseData<Boolean>>
+	fun find(id: Long): ResponseEntity<ResponseData<DETAIL_RESPONSE>>
+	fun create(req: REQ): ResponseEntity<ResponseData<DETAIL_RESPONSE>>
+	fun update(id: Long, req: REQ): ResponseEntity<ResponseData<DETAIL_RESPONSE>>
+	fun delete(id: Long): ResponseEntity<ResponseData<Boolean>>
 
-    fun getEnv(): Environment
+	fun getEnv(): Environment
 
-    fun debug(): Boolean = getEnv().activeProfiles.contains("prod").not()
+	fun debug(): Boolean = getEnv().activeProfiles.contains("prod").not()
+
+}
+
+interface CrudControllerV7<REQ, BRIEF_RESPONSE, DETAIL_RESPONSE> {
+	fun search(
+		fromDate: Instant?, toDate: Instant?,
+		query: String?,
+		page: Int, size: Int,
+		sortBy: String,
+		direction: Sort.Direction,
+		extra: Map<String, String>
+	): ResponseEntity<ResponseData<Page<BRIEF_RESPONSE>>>
+
+	fun find(id: Long): ResponseEntity<ResponseData<DETAIL_RESPONSE>>
+	fun create(req: REQ): ResponseEntity<ResponseData<DETAIL_RESPONSE>>
+	fun update(id: Long, req: REQ): ResponseEntity<ResponseData<DETAIL_RESPONSE>>
+	fun delete(id: Long): ResponseEntity<ResponseData<Boolean>>
+
+	fun getEnv(): Environment
+
+	fun debug(): Boolean = getEnv().activeProfiles.contains("prod").not()
 
 }
 
